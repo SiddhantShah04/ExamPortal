f1469c839c4990e1aa5d5991d57c50b5c596deb5
sidd044 <Siddhantshah04@gmail.com>
2019-10-20 15:33:34 +0000

    fine

--- application.py
+++ application.py
@@ -42,33 +42,36 @@ def ProfessorZone():
         return render_template("Professors.html")
 
 
-
 @app.route('/Email/<string:Email>')
 def Email(Email):
     return render_template("Professors.html",Email=Email)
 
 @app.route("/Create_Question/<string:Email>",methods=["POST","GET"])
 def Create_Question(Email):
-
     return render_template("question.html",Email=Email)
+
+
+
 
 @app.route("/Create_Paper/<string:Email>",methods=["POST","GET"])
 def Create_Paper(Email):
+
     Branch = request.form.get("Branch")
     Sem = request.form.get("Sem")
     NumberOfQuestion = int(request.form.get("NumberOfQuestions"))
     Marks = request.form.get("Marks")
     SubjectName  = request.form.get("SubjectName")
     QuestionPaperCode = request.form.get("QuestionPaperCode")
-
     try:
-        con = db.connect(f'{Email}.db')
-        cur = con.cursor()
-        cur.execute('create table Paper (Branch charr(2),sem int(2),NumberofQuestion int(4),Marks int(5),SubjectName varchar(100),QuestionPaperCode int(6))')
+        con2 = db.connect(f'{Email}.db')
+        cur2 = con2.cursor()
+        cur2.execute('create table Paper (Branch charr(2),sem int(2),NumberofQuestion int(4),Marks int(5),SubjectName varchar(100),QuestionPaperCode int(6))')
+
     except:
         print()
-    cur.execute('insert into Paper values(?,?,?,?,?,?)',(Branch,Sem,NumberOfQuestion,Marks,SubjectName,QuestionPaperCode))
-    con.commit()
+    cur2.execute('insert into Paper values(?,?,?,?,?,?)',(Branch,Sem,NumberOfQuestion,Marks,SubjectName,QuestionPaperCode))
+    con2.commit()
+
     return render_template("Paper.html",Email=Email,Branch=Branch,Sem=Sem,NumberOfQuestion=NumberOfQuestion,Marks=Marks,QuestionPaperCode=QuestionPaperCode,SubjectName=SubjectName)
 
 
